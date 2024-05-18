b5fe8bbf711228cac0308a9d1c06bcd77d4d736b
Tyler Barlow <barlowtyler96@gmail.com>
2024-05-18 18:50:40 -0400

    Add RecipeData layer

---
+++ RecipeLibraryEFCore/RecipeLibraryEFCore/DataAccess/RecipeData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeLibraryEFCore.Models;
+namespace RecipeLibraryEFCore.DataAccess;
+
+public class RecipeData(RecipeContext context)
+{
+    private readonly RecipeContext _context = context;  
+
+    //GET
+    public async Task<PaginationResponse<List<Recipe>>> GetAllRecipes(int currentPageNumber, int pageSize)
+    {
+        int skip = (currentPageNumber - 1) * pageSize;  
+        int take = pageSize;
+
+        
+        throw new NotImplementedException();
+    }
+
+    //GET
+    public async Task<Recipe> GetById(int id)
+    {
+        var recipe = await _context.Recipes
+        .FirstOrDefaultAsync(r => r.Id == id);
+        return recipe;
+    }
+
+    //GET
+    public async Task<PaginationResponse<List<Recipe>>> GetByDate(int currentPageNumber, int pageSize)
+    {
+        int skip = (currentPageNumber - 1) * pageSize;
+        int take = pageSize;
+
+        throw new NotImplementedException();
+    }
+
+    public async Task<PaginationResponse<List<Recipe>>> GetByKeyword(string keyword, int currentPageNumber, int pageSize)
+    {
+        int skip = (currentPageNumber - 1) * pageSize;
+        int take = pageSize;
+
+        throw new NotImplementedException();
+    }
+}
