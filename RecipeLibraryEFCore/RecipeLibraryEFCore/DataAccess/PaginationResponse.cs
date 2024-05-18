99eff021f11e7a7ba5a273c35f703a243f579d9e
Tyler Barlow <barlowtyler96@gmail.com>
2024-05-18 18:51:01 -0400

    Add PaginationResponse

---
+++ RecipeLibraryEFCore/RecipeLibraryEFCore/DataAccess/PaginationResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace RecipeLibraryEFCore.DataAccess;
+
+public class PaginationResponse<T> where T : class
+{
+    public int TotalCount { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int CurrentPageNumber { get; set; }
+
+    public int TotalPages { get; set; }
+
+    public T Data { get; set; }
+
+    public PaginationResponse(int totalCount, int pageSize, int currentPageNumber, T data)
+    {
+        TotalCount = totalCount;
+        CurrentPageNumber = currentPageNumber;
+        PageSize = pageSize;
+        Data = data;
+
+        TotalPages = (int)Math.Ceiling((double)TotalCount / (double)PageSize);
+    }
+}
