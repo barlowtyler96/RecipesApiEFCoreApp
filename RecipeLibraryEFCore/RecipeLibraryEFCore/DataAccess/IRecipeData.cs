4073f79f494521b3fca30e94249b2fdc4b0c1afa
Tyler Barlow <barlowtyler96@gmail.com>
2024-05-19 16:27:49 -0400

    Add IRecipeData interface

---
+++ RecipeLibraryEFCore/RecipeLibraryEFCore/DataAccess/IRecipeData.cs
@@ -0,0 +1,13 @@
+﻿using RecipeLibraryEFCore.Models;
+
+namespace RecipeLibraryEFCore.DataAccess
+{
+    public interface IRecipeData
+    {
+        Task<PaginationResponse<List<Recipe>>> GetAllRecipes(int currentPageNumber, int pageSize);
+        Task<PaginationResponse<List<Recipe>>> GetByDate(int currentPageNumber, int pageSize);
+        Task<Recipe> GetById(int id);
+        Task<PaginationResponse<List<Recipe>>> GetByKeyword(string keyword, int currentPageNumber, int pageSize);
+        Task AddRecipeAsync();
+    }
+}
