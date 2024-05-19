acadaddea119b935067db2aa89949700a87d536c
Tyler Barlow <barlowtyler96@gmail.com>
2024-05-19 16:30:14 -0400

    Add extension methods for builder

---
+++ RecipesApiEFCore/StartupConfig/DependencyInjectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using RecipeLibraryEFCore.DataAccess;
+
+namespace RecipesApiEFCore.DependencyInjectionExtentions;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddCustomServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IRecipeData, RecipeData>();
+    }
+
+    public static void AddStandardServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+    }
+}
