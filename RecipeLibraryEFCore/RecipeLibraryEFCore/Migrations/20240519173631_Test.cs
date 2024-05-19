a11cabb58bf04cddb36844d29c58b2c511bd4868
Tyler Barlow <barlowtyler96@gmail.com>
2024-05-19 16:28:54 -0400

    Add migrations

---
+++ RecipeLibraryEFCore/RecipeLibraryEFCore/Migrations/20240519173631_Test.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeLibraryEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserFavorites",
+                table: "UserFavorites");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserFavorites_UserId",
+                table: "UserFavorites");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RecipeIngredients",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeIngredients_RecipeId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserFavorites",
+                table: "UserFavorites",
+                columns: new[] { "UserId", "RecipeId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RecipeIngredients",
+                table: "RecipeIngredients",
+                columns: new[] { "RecipeId", "IngredientId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavorites_RecipeId",
+                table: "UserFavorites",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredients_IngredientId",
+                table: "RecipeIngredients",
+                column: "IngredientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserFavorites",
+                table: "UserFavorites");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserFavorites_RecipeId",
+                table: "UserFavorites");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RecipeIngredients",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeIngredients_IngredientId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserFavorites",
+                table: "UserFavorites",
+                columns: new[] { "RecipeId", "UserId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RecipeIngredients",
+                table: "RecipeIngredients",
+                columns: new[] { "IngredientId", "RecipeId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavorites_UserId",
+                table: "UserFavorites",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredients_RecipeId",
+                table: "RecipeIngredients",
+                column: "RecipeId");
+        }
+    }
+}
---
+++ RecipeLibraryEFCore/RecipeLibraryEFCore/Migrations/20240519174653_ChangeAmountDataType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeLibraryEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAmountDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "RecipeIngredients",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Amount",
+                table: "RecipeIngredients",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
